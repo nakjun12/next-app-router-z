3f45bac38135ea739606173141cfd0bcb68cfa70
nakjun <111031253+nakjun12@users.noreply.github.com>
2024-01-18 12:44:56 +0900

    feat : require 설명 추가

--- ch3-1/src/app/_component/MSWComponent.tsx
+++ ch3-1/src/app/_component/MSWComponent.tsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 
 export const MSWComponent = () => {
   useEffect(() => {
-    if (typeof window !== 'undefined') {
+    if (typeof window !== "undefined") {
       if (process.env.NEXT_PUBLIC_API_MOCKING === "enabled") {
         require("@/mocks/browser");
       }
@@ -12,3 +12,7 @@ export const MSWComponent = () => {
 
   return null;
 };
+
+//동적 로딩: require는 런타임에 호출됩니다.
+//이는 코드의 실행 도중에 모듈을 불러올 수 있다는 의미이며,
+//따라서 조건부 로딩이나 런타임 중 모듈 로딩이 가능합니다.
